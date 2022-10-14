a33b0d4fee691360405136d14b55af0233b81926
Mark Dominus <mjd@leapyear.io>
2022-10-14 08:20:52 -0400

    part of AST module; start test suite

--- src/AST.hs
+++ src/AST.hs
@@ -1,4 +1,25 @@
 module AST where
+    
+import Data.List (intercalate)
 
-data OP a = OP { opName :: String, opFunc :: a -> a -> a }
-data AST a = Con a | Op (OP a) (AST a) (AST a)
+data Operator a = Operator { opName :: String, opFunc :: a -> a -> a }
+
+opPlus :: Num a => Operator a
+opPlus = Operator { opName="+", opFunc=(+) }
+
+
+instance Show (Operator a) where
+    show (Operator {opName=n}) = "Op(" ++ n ++ ")"
+
+instance Eq (Operator a) where
+    o1 == o2   = opName o1 == opName o2
+
+data (Num a, Show a) => AST a = Con a | Op (Operator a) (AST a) (AST a)
+  deriving (Eq, Show)
+
+pprint :: (Num a, Show a) => AST a -> String
+pprint (Con a) = show a
+pprint (Op op lt rt) = intercalate " " ["(", pprint lt, opName op, pprint rt, ")"]
+
+
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,17 @@
+
+import Test.Tasty
+import Test.Tasty.HUnit
+import AST (pprint, AST(..), opPlus)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "show" $ do
+    (ast, x) <- [
+        (Con 3, "3"),
+        (Op opPlus (Con 2) (Con 2), "( 2 + 2 )"),
+        (Op opPlus (Op opPlus (Con 2) (Con 3)) (Con 4), "( ( 2 + 3 ) + 4 )"),
+        (Op opPlus (Con 1) (Op opPlus (Con 2) (Con 3)), "( 1 + ( 2 + 3 ) )")
+       ] :: [(AST Int, String)]
+    [testCase x $ pprint ast @?= x]
