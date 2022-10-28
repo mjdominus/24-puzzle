7538f16f9a6dc25506e2cf6a7d1dcc64f0bed956
Mark Dominus <mjd@leapyear.io>
2022-10-28 10:08:26 -0400

    first cut at Expr (expressions as ASTs) module

---
+++ src/Expr.hs
@@ -0,0 +1,19 @@
+module Expr (Expr (..)) where
+
+data Op = P | M | T | D
+
+data Expr a = XCon a | Expr Op (Expr a) (Expr a)
+
+instance Num a => Num (Expr a) where
+    (+) = Expr P
+    (-) = Expr M
+    (*) = Expr T
+    fromInteger = XCon . fromInteger
+    negate (XCon c) = XCon (negate c)
+    negate e = Expr M e (XCon (-1))
+    signum = undefined
+    abs = undefined
+
+instance Fractional a => Fractional (Expr a) where
+    (/) = Expr D
+    fromRational = XCon . fromRational
