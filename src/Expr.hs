0cbeacf9fd76e30a696631a6bc2bed1ffb4162d1
Mark Dominus <mjd@leapyear.io>
2022-10-28 11:21:30 -0400

    utililty functions for Exprs
    mkCon : exportable version of XCon constructor
    toStr : pretty-print Expr like "( 1 + ( 2 * 3 ) )"
    evalExpr : evaluate Expr to number

--- src/Expr.hs
+++ src/Expr.hs
@@ -1,8 +1,25 @@
-module Expr (Expr (..), Op (..)) where
+module Expr (Expr (..), Op (..), evalExpr, mkCon, toStr) where
+
+import Data.List (intercalate)
 
 data Op = P | M | T | D
+    deriving (Eq, Show)
+
+opSymbolFor :: Op -> String
+opSymbolFor P = "+"
+opSymbolFor M = "-"
+opSymbolFor T = "*"
+opSymbolFor D = "/"
 
 data Expr a = XCon a | Expr Op (Expr a) (Expr a)
+    deriving (Eq, Show)
+
+mkCon :: a -> Expr a
+mkCon = XCon
+
+toStr :: Show a => Expr a -> String
+toStr (XCon a) = show a
+toStr (Expr o a b) = intercalate " " ["(", toStr a, opSymbolFor o, toStr b, ")"]
 
 instance Num a => Num (Expr a) where
     (+) = Expr P
@@ -17,3 +34,10 @@ instance Num a => Num (Expr a) where
 instance Fractional a => Fractional (Expr a) where
     (/) = Expr D
     fromRational = XCon . fromRational
+-- can I use this for ExprToEzpr?
+evalExpr :: (Real a, Fractional b) => Expr a -> b
+evalExpr (XCon c) = fromRational $ toRational c
+evalExpr (Expr P a b) = evalExpr a + evalExpr b
+evalExpr (Expr M a b) = evalExpr a - evalExpr b
+evalExpr (Expr T a b) = evalExpr a * evalExpr b
+evalExpr (Expr D a b) = evalExpr a / evalExpr b
