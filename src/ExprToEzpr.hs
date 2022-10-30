76961e49d096d2563a962c39bf5c350200d746f5
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2022-10-29 20:49:00 -0400

    loosen constraint

--- src/ExprToEzpr.hs
+++ src/ExprToEzpr.hs
@@ -6,7 +6,7 @@ import Ezpr
 -- can I just use evalExpr here?
 -- not without some work it seems
 -- the sticking point seems to be: how to convert XCon c -> t ?
-toEzpr :: (Fractional a, Eq a) => Expr a -> Ezpr a
+toEzpr :: (Num a, Eq a) => Expr a -> Ezpr a
 -- toEzpr = evalExpr
 toEzpr (XCon c) = Con c
 toEzpr (Expr P a b) = toEzpr a + toEzpr b
