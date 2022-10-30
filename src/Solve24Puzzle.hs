4188c04791cd6e058a1e8c31f521b47ce99416f5
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2022-10-29 20:51:24 -0400

    Solve24Puzzle.solve now produces Ezprs
    For Exprs, use rawSolve instead

--- src/Solve24Puzzle.hs
+++ src/Solve24Puzzle.hs
@@ -1,14 +1,21 @@
-module Solve24Puzzle (solve, solve24) where
+module Solve24Puzzle (rawSolve, solve, solve24) where
 
 import Expr (Expr, evalExpr, mkCon)
+import ExprToEzpr (toEzpr)
+import Ezpr (Ezpr)
 
 -- import ExprToEzpr ()
 
-solve24 :: [Rational] -> [Expr Rational]
-solve24 = solve 24
+solve24 :: Real a => [a] -> [Ezpr a]
+solve24 = solve (24 :: Rational)
 
-solve :: Rational -> [Rational] -> [Expr Rational]
-solve target pool = filter ((== target) . evalExpr) (allExpressions (map mkCon pool))
+-- solve :: Rational -> [Rational] -> [Expr Rational]
+-- solve :: Rational -> [Rational] -> [Expr Rational]
+solve :: (Real a, Fractional b, Eq b) => b -> [a] -> [Ezpr a]
+solve target pool = nub $ map toEzpr $ rawSolve target pool
+
+rawSolve :: (Real a, Fractional b, Eq b) => b -> [a] -> [Expr a]
+rawSolve target pool = filter ((== target) . evalExpr) (allExpressions (map mkCon pool))
 
 allExpressions :: Fractional a => [a] -> [a]
 allExpressions [] = []
