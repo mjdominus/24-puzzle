01a54d73d01ff22962f98d06215465bf9467d5ec
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2022-10-29 20:53:12 -0400

    example main program for development

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
 module Main (main) where
 
-import AST()
+import Data.List (nub)
+import qualified Expr (toStr)
+import ExprToEzpr (toEzpr)
+import qualified Ezpr (Ezpr, toStr)
+import Solve24Puzzle (rawSolve, solve)
 
 main :: IO ()
-main = return ()
+main = do
+    let s = rawSolve 24 [1, 2, 7, 7]
+    putStrLn $ "Found " ++ show (length s) ++ " solutions"
+    let ss = map (\x -> (Expr.toStr x ++ " -> " ++ (Ezpr.toStr $ toEzpr x))) s
+    putStrLn $ unlines ss
+    let sols = nub $ map toEzpr s
+    putStrLn $ "No, actually only " ++ show (length sols) ++ " solutions"
+    putStrLn $ unlines (map Ezpr.toStr sols)
